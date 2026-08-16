fbeb651578e91f5cef34d6e5a94447f9bbffd9ad
pavelbekarev <bekarev.ps@gmail.com>
2026-08-16 18:07:52 +0500

    feat: implemented MainBanner

---
+++ src/components/MainBanner/index.ts
@@ -0,0 +1,3 @@
+import MainBanner from "./ui/MainBanner.vue";
+
+export default MainBanner;
--- src/pages/Home/ui/Home.vue
+++ src/pages/Home/ui/Home.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import Header from '../../../components/Header';
+import MainBanner from '../../../components/MainBanner';
 </script>
 <template>
     <Header />
     <main>
-        
+        <MainBanner />
     </main>
 </template>
-
