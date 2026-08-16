afdf73866ac291133e0497fa46d9ba31c9375c1b
pavelbekarev <bekarev.ps@gmail.com>
2026-08-16 15:32:19 +0500

    feat: install scss and add HomePage component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import Home from './pages/Home/ui/Home.vue';
 </script>
 
 <template>
+    <Home />
 </template>
---
+++ src/pages/Home/index.ts
@@ -0,0 +1,3 @@
+import Home from "./ui/Home.vue";
+
+export default Home;
